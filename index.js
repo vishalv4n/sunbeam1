40786364456518e6d5d14c72fd83c870cb4f326d
vishalvilas nikam <vishalvilasv4n@gmail.com>
2019-11-13 15:32:28 +0530

    /college is addeded

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ response.send("hello world");
 app.use("/student",(request,response)=>{
     response.send("hello future");
     });
+    app.use("/college",(request,response)=>{
+        response.send("list of college");
+        });
 app.listen(4000,()=>{
     console.log("server started");
 })
